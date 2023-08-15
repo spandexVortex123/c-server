1c2ddaf4fd6afa96fd4069e1ea0cef6da7601e07
user <user@localhost.localdomain>
2023-08-16 02:49:02 +0530

    removed multiple files, only single file server

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all:
-	gcc main.c init.c utils.c -o main -Wall -Wextra
+	gcc main.c -lpthread -o main -Wall -Wextra
 
 clean:
 	rm main
--- main.c
+++ main.c
@@ -1,54 +1,149 @@
-#include "init.h"
-#include "utils.h"
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <time.h>
+#include <errno.h>
+#include <pthread.h>
+#include <arpa/inet.h>
+#define DEBUG 1
+#define INFO 2
+#define WARN 3
+#define ERROR 4
+#define MAX_BUFFER_SIZE 65535
+#define SO_REUSEPORT 15
+#define PORT 8000
+
+struct Request {
+    int new_s;
+    struct sockaddr_in *client;
+};
+
+void setup();
+void *handle_request(void *args);
 
 void setup() {
-    int s = create_socket();
-    char* buffer = (char*)malloc(50);
-    memset(buffer, 0x0, 50);
-    sprintf(buffer, "Socket Created With Descriptor: %d\n", s);
-    write_startup_log(buffer);
-    memset(buffer, 0x0, 50);
-    struct sockaddr_in *server = create_sockaddr_in_struct(0);
-    sprintf(buffer, "Struct sockaddr_in Initialized\n");
-    write_startup_log(buffer);
-    memset(buffer, 0x0, 50);
-    bind_socket(s, (struct sockaddr*)&(*server), sizeof(struct sockaddr));
-    sprintf(buffer, "Binding socket to all interfaces at port 8000\n");
-    write_startup_log(buffer);
-    memset(buffer, 0x0, 50);
-    listen_server(s, 3);
-    sprintf(buffer, "Listening on port 8000\n");
-    write_startup_log(buffer);
-    memset(buffer, 0x0, 50);
+    // create socket
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock < 0) {
+        perror("Error creating socket.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // set socket options REUSEADDR
+    int reuse = 1;
+    int set_socket_options_addr_result = setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+    if(set_socket_options_addr_result != 0) {
+        perror("Error setting socket options for addr.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // set socket options REUSEPORT
+    int set_sock_options_port_result = setsockopt(sock, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse));
+    if(set_sock_options_port_result != 0) {
+        perror("Error setting socket options for port.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // create sockaddr_in struct
+    struct sockaddr_in server;
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(PORT);
+
+    // bind
+    int bind_result = bind(sock, (struct sockaddr*)&server, sizeof(struct sockaddr));
+    if(bind_result != 0) {
+        perror("Error binding.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // listen
+    int listen_result = listen(sock, 10);
+    if(listen_result != 0) {
+        perror("Error listening.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while(1) {
+        // accept connections
+        struct sockaddr_in *client = (struct sockaddr_in*) malloc(sizeof(struct sockaddr_in)); // need to free
+        socklen_t client_length = sizeof(*client);
+        int new_socket = accept(sock, (struct sockaddr*)&(*client), &client_length);
+        if(new_socket <= 0) {
+            perror("Error accepting connections.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        // create struct to send to thread
+        struct Request *req = (struct Request*) malloc(sizeof(struct Request)); // need to free
+        if(req == NULL) {
+            perror("Error creating req pointer.\n");
+            exit(EXIT_FAILURE);
+        }
+        req->new_s = new_socket;
+        req->client = client;
+
+        // create thread and detach it
+        pthread_t thread_id;
+        pthread_create(&thread_id, NULL, handle_request, req);
+        pthread_detach(thread_id);
+
+        /*
+        // get size of bytes from socket to read
+        int bytes_to_read = 0;
+        sleep(0.5);
+        ioctl(new_socket, FIONREAD, &bytes_to_read);
+
+        // create buffer of size bytes_to_read and read into it
+        char* buffer = (char*)malloc(bytes_to_read);
+        int bytes_read = read(new_socket, buffer, bytes_to_read);
+        printf("bytes read: %d\n", bytes_read);
+        printf("Received data: \n\n%s\n\n", buffer);
+        free(buffer);
+
+        // send response back
+        char* message = "hello\n";
+        sleep(10);
+        write(new_socket, message, 6);
+        close(new_socket);
+        */
+    }
 }
 
-void loop() {
-    printf("------------START------------\n");
-    int s = create_socket();
-    struct sockaddr_in *server = create_sockaddr_in_struct(0); // need to free
-    bind_socket(s, (struct sockaddr*)&(*server), sizeof(struct sockaddr));
-    listen_server(s, 3);
-    struct sockaddr_in *client = create_sockaddr_in_struct(1); // need to free
-    socklen_t client_size = sizeof(*client);
-    int new_s = accept_connection(s, (struct sockaddr*)&(*client), client_size);
-    int read_size = get_size_of_data(new_s);
-    char* buffer = (char*) malloc(read_size);
-    memset(buffer, 0x0, read_size);
-    read_data(new_s, buffer, read_size);
-    printf("Data Read: \n\n%s\n\n", buffer);
-    // parse request
-    char* sample_message = "Hello World, Data Received\n";
-    write_data(new_s, sample_message, 27);
-    printf("-------END--------\n");
+void *handle_request(void *args) {
+    // get client struct and new socket descriptor
+    struct Request *request = (struct Request*) args;
+    int new_socket = request->new_s;
+    struct sockaddr_in *client = request->client;
+    // get client information
+    char* ip_addr = inet_ntoa(client->sin_addr);
+    unsigned int port = ntohs(client->sin_port);
+    printf("Connection Received from %s:%d\n", ip_addr, port);
+    // get request size
+    int request_size = 0;
+    ioctl(new_socket, FIONREAD, &request_size);
+    // create buffer for request size and read into it
+    char* buffer = (char*)malloc(request_size); // need to free
+    int bytes_read = read(new_socket, buffer, request_size);
+    printf("bytes read: %d\n", bytes_read);
+    printf("Data Received: \n\n%s\n\n", buffer);
+    // send back some data
+    char* message = "data received\n";
+    sleep(10); // added sleep to check for multiple request handling
+    write(new_socket, message, 14);
+    free(buffer);
     free(client);
-    free(server);
-    close(new_s);
-    close(s);    
+    close(new_socket);
+    free(request);
+    pthread_exit(0);
 }
 
 int main() {
-    char* time = get_time();
-    printf("time now: %s\n", time);
-    free(time);
+    setup();
     return 0;
 }
